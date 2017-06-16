569c3e39cb3fdff61b658d090c6ae0d2553c087d
Leigh Steiner <steinerleigh@gmail.com>
2017-06-16 11:53:22 -0400

    structure initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
